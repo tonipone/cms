a0cbf9c8208c72d6a4c89a298694558be9fd9a6f
tonipone <terseo62@gmail.com>
2021-08-19 08:52:09 +0200

    lez 20 DB

--- core/DB.php
+++ core/DB.php
@@ -3,8 +3,10 @@
 class DB {
 	private static $_instance = null;
 	private $_pdo, $_query, $_error, $_result, $_count = 0, $_lastInsertID = null;
+	private $var = 0;
 
 	private function __construct() {
+		$this->var =1;
 		try{
 			$this->_pdo = new PDO("mysql:host=localhost;dbname=ap_mvc","root","root");
 		}catch (PDOException $e){
