a6ae11068a1d2a4461a91fd3a985a20f6798dede
tonipone <terseo62@gmail.com>
2021-08-19 09:01:11 +0200

    Lez. 20 DB 1908

--- core/DB.php
+++ core/DB.php
@@ -3,15 +3,22 @@
 class DB {
 	private static $_instance = null;
 	private $_pdo, $_query, $_error, $_result, $_count = 0, $_lastInsertID = null;
-	private $var = 0;
+
 
 	private function __construct() {
-		$this->var =1;
+
 		try{
 			$this->_pdo = new PDO("mysql:host=localhost;dbname=ap_mvc","root","root");
 		}catch (PDOException $e){
 			die($e->getMessage());
 		}
+	}
+
+	public static function getInstance(){
+		if(!isset(self::$_instance)){
+			self::$_instance = new DB();
+		}
+		return self::$_instance;
 	}
 }
 
