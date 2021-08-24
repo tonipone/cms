b8544a04f32fd4597efccd24a728ef91f340125f
tonipone <terseo62@gmail.com>
2021-08-24 09:42:41 +0200

    Lez. 27 Model-2 24AGO

--- core/Model.php
+++ core/Model.php
@@ -29,4 +29,39 @@ class Model {
 		return $this->_db->get_columns($this->_table);
 	}
 
+	public function find($params = []){
+		$results = [];
+		$resultsQuery = $this->_db->find($this->_table, $params);
+
+		foreach ( $resultsQuery as $result ) {
+			$obj = new $this->_modelName($this->_table);
+			$obj->populateObjData($result);
+			$results[] = $obj;
+		}
+
+		return $results;
+	}
+
+	public function findFirst($params = []){
+		$resultQuery = $this->_db->findFirst($this->_table, $params);
+		$result = new $this->_modelName($this->_table);
+		$result->populateObjData($resultQuery);
+		return $result;
+	}
+
+	public function findById($id){
+		return $this->findFirst(['conditions'=>"id = ?", 'bind' => [$id]]);
+	}
+
+	public function insert($fields){
+		if(empty($fields)) return false;
+		return $this->_db->insert($this->_table, $fields);
+	}
+
+	protected function populateObjData($result){
+		foreach ($result as $key => $val ) {
+			$this->key = $val;
+		}
+	}
+
 }
